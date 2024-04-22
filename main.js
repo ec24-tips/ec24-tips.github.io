e340d80bd3689e626ba9d2855c1679800a6c8aec
Luca De Feo <defeo@merry>
2024-04-23 01:13:44 +0200

    Small steps

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const map = L.map('map').setView([47.3717315, 8.5420985], 15);
 const main = document.querySelector('#main');
-const pane = document.querySelector('#pane');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,40 +7,172 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 L.control.scale().addTo(map);
 
-const catIcons = {
-    'food': L.divIcon({
-	html: 'M',
-	className: 'food',
-    }),
-};
+class Marker extends L.Marker {
+    #props;
+    #thumbnail;
+    #card;
+    
+    static
+    catIcons = {
+	'food': L.divIcon({
+	    html: 'M',
+	    className: 'food',
+	}),
+    }
+    
+    constructor(latlng, props) {
+	super(latlng, {
+	    icon: Marker.catIcons[props.category] || L.Icon.Default,
+	    alt: props.name,
+	});
+	this.#props = props;
+	this.tags = new Set(props.tags);
+	this.tags.add(props.category);
+	this.selected = this.hidden = false;
+    }
+    
+    select() {
+	this.selected = true;
+	this._icon.classList.add('selected');
+    }
+
+    unselect() {
+	this.selected = false;
+	this._icon.classList.remove('selected');
+    }
+
+    hide() {
+	this.hidden = true;
+	this._icon.classList.add('hidden');
+	this.#thumbnail.classList.add('hidden');
+    }
+
+    show() {
+	this.hidden = false;
+	this._icon.classList.remove('hidden');
+	this.#thumbnail.classList.remove('hidden');
+    }
+
+    get thumbnail() {
+	if (this.#thumbnail)
+	    return this.#thumbnail;
+	this.#thumbnail = document.createElement('div');
+	this.#thumbnail.className = 'thumbnail';
+	this.#thumbnail.innerHTML = `<h3>${this.#props.name}</h3>`;
+	return this.#thumbnail;
+    }
+
+    get card() {
+	if (this.#card)
+	    return this.#card;
+	this.#card = document.createElement('div');
+	this.#card.className = 'card';
+	this.#card.innerHTML = `<h3>${this.#props.name}</h3>${this.#props.description}`;
+	return this.#card;
+    }
+}
+
+class MarkerCollection extends Array {
+    #tags = new Map();
+    
+    tag(key) {
+	let markers = this.#tags.get(key);
+	if (!markers) {
+	    markers = new Array();
+	    this.#tags.set(key, markers);
+	}
+	return markers;
+    }
+
+    insert(marker) {
+	this.push(marker);
+	for (let m of marker.tags)
+	    this.tag(m).push(marker);
+    }
+}
+
+map._drawer = new class {
+    #activeMarker;
+    #markers = new MarkerCollection();
+    
+    constructor(container, map) {
+	this.container = container;
+	this.map = map;
+	this.pane = this.container.querySelector('#pane');
+	this._open = false;
+	
+	map.on('click', (e) => this.close());
+	this.container.addEventListener('transitionend',
+					(e) => this.map.invalidateSize());
+    }
+    
+    addMarker(latlng, props) {
+	const m = new Marker(latlng, props);
+	m.on('click', (e) => this.open(m));
+	this.#markers.insert(m);
+	return m;
+    }
+
+    open(marker) {
+	if (marker) {
+	    if (this.#activeMarker)
+		this.#activeMarker.unselect()
+	    marker.select()
+	    this.renderPane(marker);
+	    if (!this._open)
+		this.map.once('resize', () => this.map.panTo(marker.getLatLng()));
+	    else
+		this.map.panTo(marker.getLatLng())
+	    this.#activeMarker = marker;	
+	}
+	
+	this.container.classList.add('open');
+	this._open = true;
+    }
+
+    close() {
+	this.container.classList.remove('open');
+	this._open = false;
+	
+	if (this.#activeMarker)
+	    this.#activeMarker.unselect();
+	this.#activeMarker = null;
+    }
+
+    hideAll() {
+	this.#markers.forEach((m) => m.hide());
+    }
+
+    showAll() {
+	this.#markers.forEach((m) => m.show());
+    }
+    
+    hide(tag) {
+	this.#markers.tag(tag).forEach((m) => m.hide());
+    }
+    
+    show(tag) {
+	this.#markers.tag(tag).forEach((m) => m.show());	
+    }
+
+    renderPane(marker) {
+	this.pane.replaceChildren(marker.card);
+    }
+
+    render() {
+	
+    }
+}(main, map);
 
 (async function() {
     const res = await fetch("features.json");
     const features = await res.json();
 
     L.geoJSON(features, {
-	pointToLayer: (point, latlng) => {
-	    const p = point.properties;
-	    const m = L.marker(latlng, {
-		icon: catIcons[p.category] || L.Icon.Default,
-		alt: p.name,
-	    });
-	    m.on('click', (e) => {
-		main.classList.add('open');
-		pane.innerHTML = `<h3>${p.name}</h3>${p.description}`;
-		map.once('resize', () => map.panTo(m.getLatLng()));
-	    });
-	    return m;
-	}
+	pointToLayer: (point, latlng) => map._drawer.addMarker(latlng, point.properties),
     }).addTo(map);
 })();
 
-map.on('click', (e) => {
-    main.classList.remove('open');
-});
-main.addEventListener('transitionend', (e) => {
-    map.invalidateSize();    
-});
 
 /* Geolocation */
 /*
