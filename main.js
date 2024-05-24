2c9a538cef2f72c76cc3cc9a8adfc3fd912d0fdb
Luca De Feo <defeo@merry>
2024-05-24 02:02:14 +0200

    Tag-based filtering and minor fixes

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function e(strings, ...values) {
 /* This class enriches a Leaflet layer by making it interactive */
 class Interactor {
     #layer;
+    #title;
     #thumbnail;
     #card;
 
@@ -84,16 +85,29 @@ class Interactor {
 	this.#thumbnail.classList.remove('hidden');
     }
 
+    get title() {
+	if (this.#title)
+	    return this.#title;
+	this.#title = document.createElement('h3');
+	this.#title.className = `title cat-${this.props.category}`;
+
+	this.#title.innerHTML = e`
+<span class="material-symbols-outlined icon cat-${this.props.category}"></span><!--
+--><span>${this.props.name}</span>`;
+	return this.#title;
+    }
+
     get thumbnail() {
 	if (this.#thumbnail)
 	    return this.#thumbnail;
-	this.#thumbnail = document.createElement('div');
+	this.#thumbnail = document.createElement('a');
 	this.#thumbnail.className = `thumbnail cat-${this.props.category}`;
-	this.#thumbnail.innerHTML = e`<h3>${this.props.name}</h3>`;
+	this.#thumbnail.href = '#' + this.uid;
+	this.#thumbnail.appendChild(this.title.cloneNode(true));
 	this.#thumbnail.innerHTML += `
-<ul class="tags">
-  ${ [...this.tags.values()].map((t) => e`<li><button>#${t}</button></li>`).join('') }
-</ul>`;
+<div class="tags">
+  ${ [...this.tags.values()].map((t) => e`<span>#${t}</span></li>`).join('') }
+</div>`;
 	return this.#thumbnail;
     }
 
@@ -103,16 +117,6 @@ class Interactor {
 	this.#card = document.createElement('div');
 	this.#card.className = `card cat-${this.props.category}`;
 
-	this.#card.innerHTML = e`
-<h3>
-  <span class="material-symbols-outlined icon cat-${this.props.category}"></span>
-  <span>${this.props.name}</span>
-</h3>`;
-	
-	this.#card.innerHTML += `
-<ul class="tags">
-  ${ [...this.tags.values()].map((t) => e`<li><button>#${t}</button></li>`).join('') }
-</ul>`;
 	this.#card.innerHTML += `<div class="description">${this.props.description}</div>`;
 	
 	if (this.props.recommenders)
@@ -163,7 +167,7 @@ class LayerCollection extends Map {
 	keys: ['value'],
     });
     
-    tag(key) {
+    #tagOrCreate(key) {
 	let layers = this.#tags.get(key);
 	if (!layers) {
 	    layers = new Array();
@@ -176,6 +180,10 @@ class LayerCollection extends Map {
 	return layers;
     }
 
+    tag(key) {
+	return this.#tags.get(key);
+    }
+
     tags() {
 	return this.#tags.keys();
     }
@@ -183,7 +191,7 @@ class LayerCollection extends Map {
     insert(layer) {
 	this.set(layer.I.uid, layer);
 	for (let m of layer.I.tags)
-	    this.tag(m).push(layer);
+	    this.#tagOrCreate(m).push(layer);
 	this.#index.add({
 	    type: 'layer',
 	    layer: layer,
@@ -237,8 +245,7 @@ class Geolocalization extends L.Control {
 	return `
 <a class="material-symbols-outlined ${this.state.name}" role="button"
    title="${this.state.desc}" aria-label="${this.state.desc}"
-   aria-disabled="false">
-</a>`;
+   aria-disabled="false"></a>`;
     }
 
     onAdd(map){
@@ -260,7 +267,7 @@ class Geolocalization extends L.Control {
 	});
 	this._map.on('locationfound', this.onLocation, this);
 	this._map.on('locationerror', this.onError, this);
-	this._map.on('move', this.onMove, this);
+	this._map.on('movestart', this.onMove, this);
 	return this.button;
     }
     
@@ -292,19 +299,20 @@ class Geolocalization extends L.Control {
     }
 
     onMove(e) {
-	// Only cancel tracking on map drag
-	if (e.originalEvent && this.#state === 'T') {
+	// Only cancel tracking on user move
+	if (this.#state === 'T') {
 	    this.#state = 'A';
 	    this.button.innerHTML = this.icon();
 	}
     }
 
     panToLocation() {
-	this._map.panTo(this.position.getLatLng())
+	// Use noMoveStart to avoid triggering onMove
+	this._map.panTo(this.position.getLatLng(), { noMoveStart: true })
     }
 }
 
-const app = new class {
+const app = new class extends L.Control {
     #layers = new LayerCollection();
     #localization = new Geolocalization({ position: "topleft" });
     #state = {
@@ -317,17 +325,27 @@ const app = new class {
 	if (state?.pane_open !== undefined)
 	    this.#state.pane_open = state.pane_open;
 	if (state?.activeTags !== undefined)
-	    this.#state.activeTags = state.activeTags;
+	    this.activeTags = state.activeTags;
 	if (state?.activeLayer !== undefined)
 	    this.activeLayer = this.#layers.get(state.activeLayer) ?? null;
     }
 
-    #setStateFromUID(uid) {
-	this.#setState({ activeLayer: uid });
-	this.#state.pane_open = Boolean(this.#state.activeLayer);
+    #setStateFromHash(hash) {
+	if (hash.startsWith('#t=')) {
+	    this.#setState({
+		activeLayer: null,
+		activeTags: decodeURIComponent(hash).substr(3).split(','),
+		pane_open: true,
+	    });
+	} else {
+	    const uid = hash.substr(1);
+	    this.#setState({ activeLayer: uid });
+	    this.#state.pane_open = Boolean(this.#state.activeLayer);
+	}
     }
     
     constructor(container, features) {
+	super({ position: 'topright' });
 	this.DOM = {};
 	this.DOM.container = container;
 	this.populateDOM();
@@ -336,7 +354,7 @@ const app = new class {
 	window.addEventListener('popstate', (e) => this.restoreState(e.state));
 	this.populateMap(features).then(() => {
 	    if (location.hash)
-		this.#setStateFromUID(location.hash.substr(1));
+		this.#setStateFromHash(location.hash);
 	    this.reflectState();
 	    history.replaceState(this.#state, "");
 	});
@@ -346,23 +364,39 @@ const app = new class {
 	this.DOM.container.innerHTML = `<div id="map"></div>
 <div id="pane">
   <div id="pane-content">
-    <div id="share-button" class="material-symbols-outlined">&#xe80d;</div>
-    <div id="list">
-      <input id="search" list="search-results" type="search" />
-      <datalist id="search-results"></datalist>
-      <div id="active-tags"></div>
-      <div id="layers"></div>
+    <div id="pane-header">
+      <div id="back-button" class="show-on-result material-symbols-outlined">&#xe5c4;</div>
+      <div id="pane-control">
+        <div class="show-on-list">
+          <input id="search" list="search-results" type="search" style="display:none" />
+          <datalist id="search-results"></datalist>
+          <div id="alltags"></div>
+        </div>
+        <div class="show-on-result">
+          <div id="title"></div>
+          <div id="tags"></div>
+        </div>
+      </div>
+      <div id="share-button" class="material-symbols-outlined">&#xe80d;</div>
     </div>
-    <div id="result"></div>
+    <div id="layers" class="show-on-list"></div>
+    <div id="result" class="show-on-result"></div>
   </div>
 </div>`;
 
-	this.DOM.list = this.DOM.container.querySelector('#list');
+	this.DOM.search = this.DOM.container.querySelector('#search');
+	this.DOM.search_results = this.DOM.container.querySelector("#search-results");
+	this.DOM.alltags = this.DOM.container.querySelector('#alltags');
+	this.DOM.title = this.DOM.container.querySelector('#title');
+	this.DOM.tags = this.DOM.container.querySelector('#tags');
+	this.DOM.layers = this.DOM.container.querySelector('#layers');
 	this.DOM.result = this.DOM.container.querySelector('#result');
-	this.DOM.search = this.DOM.list.querySelector('#search');
-	this.DOM.search_results = this.DOM.list.querySelector("#search-results");
-	this.DOM.active_tags = this.DOM.list.querySelector('#active-tags');
-	this.DOM.layers = this.DOM.list.querySelector('#layers');
+	
+	this.DOM.alltags.addEventListener('click', (e) => 
+	    e.target.classList.contains('tag') && this.toggleTag(e.target));
+
+	this.DOM.container.querySelector('#back-button')
+	    .addEventListener('click', () => this.open(null));
 
 	if (navigator.share) {
 	    const share = this.DOM.container.querySelector('#share-button');
@@ -379,6 +413,8 @@ const app = new class {
 	this.map = L.map(this.DOM.container.querySelector('#map'))
 	    .setView([47.3717315, 8.5420985], 15);
 	
+	this.addTo(this.map);
+
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    maxZoom: 19,
 	    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | <img src="assets/github-mark.svg" width="10" height="10"> <a href="https://github.com/ec24-tips/ec24-tips.github.io/">Contribute</a>',
@@ -389,6 +425,23 @@ const app = new class {
 	this.DOM.container.addEventListener('transitionend',
 					(e) => this.map.invalidateSize());
 	this.#localization.addTo(this.map);
+    }
+
+    onAdd(map) {
+	this.burger = document.createElement('div');
+	this.burger.id = 'burger';
+	this.burger.className = 'leaflet-bar';
+	this.burger.innerHTML = `
+<a class="material-symbols-outlined"
+   title="Open pane" role="button" aria-label="Open pane"
+   aria-disabled="false">
+   <span aria-hidden="true">&#xe5d2</span>
+</a>`;
+	this.burger.addEventListener('click', (e) => {
+	    this.toggle();
+	    e.stopPropagation();
+	});
+	return this.burger;
     }
 
     activateSearch() {
@@ -444,9 +497,51 @@ const app = new class {
 	}
     }
 
+    tagFilterClear() {
+	for (const [id, l] of this.#layers)
+	    l.I.show();
+	this.#state.activeTags = [];
+    }
+
+    tagFilterAdd(tag) {
+	const layers = this.#layers.tag(tag);
+	if (layers) {
+	    if (this.#state.activeTags.length == 0)
+		for (const [id, l] of this.#layers)
+		    l.I.hide();
+	    for (const l of layers)
+		l.I.show();
+	    this.#state.activeTags.push(tag);
+	}
+    }
+
+    tagFilterRemove(tag) {
+	this.activeTags = this.#state.activeTags.filter((t) => t != tag);
+    }
+
+    get activeTags() {
+	return new Array(this.#state.activeTags);
+    }
+
+    set activeTags(tags) {
+	this.tagFilterClear()
+	for (const t of tags)
+	    this.tagFilterAdd(t);
+    }
+
     open(layer) {
 	this.activeLayer = layer;
 	this.#state.pane_open = true;
+	this.reflectState();
+	this.saveState();
+    }
+
+    toggleTag(tag) {
+	if (!tag.classList.contains('inactive')
+	    && this.#state.activeTags.length > 0)
+	    this.tagFilterRemove(tag.dataset['tag']);
+	else
+	    this.tagFilterAdd(tag.dataset['tag']);
 	this.reflectState();
 	this.saveState();
     }
@@ -458,17 +553,35 @@ const app = new class {
 	this.saveState();
     }
 
+    toggle() {
+	this.#state.pane_open ? this.close() : this.open(null);
+    }
+
     // Call after a state change to update UI
     reflectState() {
 	const wasClosed = !this.DOM.container.classList.contains('open');
 	if (this.DOM.container.classList.toggle('open', this.#state.pane_open)) {
 	    if (this.activeLayer) {
+		this.DOM.title.replaceChildren(this.activeLayer.I.title)
+		this.DOM.tags.replaceChildren();
+		for (const t of this.activeLayer.I.tags) {
+		    this.DOM.tags.innerHTML += `<a href="#t=${t}">#${t}</a>`;
+		}
 		this.DOM.result.replaceChildren(this.activeLayer.I.card);
 		const pan = () => this.activeLayer.I.center(this.map);
 		wasClosed ? this.map.once('resize', pan) : pan();
-		this.DOM.container.classList.add('layer-view');
+		this.DOM.container.classList.remove('list-view');
 	    } else {
-		this.DOM.container.classList.remove('layer-view');
+		this.DOM.container.classList.add('list-view');
+		this.DOM.alltags.replaceChildren();
+		for (const t of this.#layers.tags()) {
+		    const cls = this.#state.activeTags.length == 0
+			  || this.#state.activeTags.includes(t)
+			  ? '' 
+			  : 'inactive';
+		    this.DOM.alltags.innerHTML += `
+<span data-tag="${t}" class="tag ${cls}">#${t}</span>`;
+		}
 	    }
 	}
     }
@@ -476,35 +589,23 @@ const app = new class {
     // Save state in history
     saveState() {
 	if (JSON.stringify(this.#state) != JSON.stringify(history.state)) {
-	    history.pushState(this.#state, '', '#' + (this.#state.activeLayer || ''));
+	    const hash = this.#state.pane_open
+		  ? this.#state.activeLayer
+		  || `t=${this.#state.activeTags.join()}`
+		  : '';
+	    history.pushState(this.#state, '', '#' + hash);
 	}
     }
 
-    // Restore to a previous state
+    // Restore to a previous/new state
     restoreState(state) {
 	if (state === null) {
 	    // when hash changes a null state is popped
-	    this.#setStateFromUID(location.hash.substr(1));
+	    this.#setStateFromHash(location.hash);
 	    this.reflectState();
 	} else if (JSON.stringify(this.#state) != JSON.stringify(state)) {
 	    this.#setState(state);
 	    this.reflectState();
 	}
-    }
-    
-    hideAll() {
-	this.#layers.forEach((m) => m.hide());
-    }
-
-    showAll() {
-	this.#layers.forEach((m) => m.show());
-    }
-    
-    hide(tag) {
-	this.#layers.tag(tag).forEach((m) => m.hide());
-    }
-    
-    show(tag) {
-	this.#layers.tag(tag).forEach((m) => m.show());	
     }
 }(document.querySelector('#main'), "features.json");
